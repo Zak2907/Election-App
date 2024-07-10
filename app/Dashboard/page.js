3acd543afc685d51dbc8e54b78116cfbc7636c66
Zak2907 <zakariamohamed2907>
2024-07-10 17:35:04 -0500

    Color again + dashboard

---
+++ app/Dashboard/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Dashboard = () => {
+    return (
+        <div>
+            <h1><center>Dashboard</center></h1>
+            <p><center>Welcome to the Dashboard.</center></p>
+            {}
+        </div>
+    );
+};
+export default Dashboard;
---
+++ app/homePage/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const homePage = () => {
+    return (
+        <div>
+            <h1><center>Home Page</center></h1>
+            <p><center>Introducing our new election app, designed to revolutionize the way you engage with the electoral process. This user-friendly app offers a range of features to simplify voting for users of all levels of experience. Stay informed with candidate profiles, ballot details, and real-time election updates. Easily locate your polling place and access personalized voting reminders. Securely register to vote and request absentee ballots with confidence in our robust security measures. With our election app, participating in democracy has never been more convenient or accessible. Join us in shaping the future of our communities with just a few taps on your device.</center></p>
+            {}
+        </div>
+    );
+};
+export default homePage;
